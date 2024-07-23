8b9bc8c07f43be2a646e33838ef0dca02263b857
Pam Patacsil <74691394+patacsilpam@users.noreply.github.com>
2024-07-23 16:04:41 +0800

    forms

--- resources/js/Pages/User/Create.jsx
+++ resources/js/Pages/User/Create.jsx
@@ -5,15 +5,39 @@ import React from "react";
 const CreateUser = () => {
     return (
         <div>
-            <form>
-                <InputLabel value="First Name" />
-                <TextInput
-                    id="fname"
-                    name="fname"
-                    type="text"
-                    className="mt-1 block w-full"
-                    placeholder="Enter first name"
-                />
+            <form className="flex flex-col w-full">
+                <div className="grid lg:grid-cols-3 sm:grid-cols-1 gap-2">
+                    <div>
+                        <InputLabel value="First Name" />
+                        <TextInput
+                            id="fname"
+                            name="fname"
+                            type="text"
+                            className="mt-1 block w-full"
+                            placeholder="Enter first name"
+                        />
+                    </div>
+                    <div>
+                        <InputLabel value="First Name" />
+                        <TextInput
+                            id="fname"
+                            name="fname"
+                            type="text"
+                            className="mt-1 block w-full"
+                            placeholder="Enter first name"
+                        />
+                    </div>
+                    <div>
+                        <InputLabel value="First Name" />
+                        <TextInput
+                            id="fname"
+                            name="fname"
+                            type="text"
+                            className="mt-1 block w-full"
+                            placeholder="Enter first name"
+                        />
+                    </div>
+                </div>
             </form>
         </div>
     );
