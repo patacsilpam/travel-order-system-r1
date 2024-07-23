824358ab8c9960493b9b8716dafa4b9d9ce636f1
Pam Patacsil <74691394+patacsilpam@users.noreply.github.com>
2024-07-23 10:45:22 +0800

    feat: improve code

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -3,20 +3,10 @@ import ApplicationLogo from "@/Components/ApplicationLogo";
 import Dropdown from "@/Components/Dropdown";
 import NavLink from "@/Components/NavLink";
 import ResponsiveNavLink from "@/Components/ResponsiveNavLink";
-import { Link } from "@inertiajs/react";
-import {
-    CircleGauge,
-    FilePen,
-    Compass,
-    Users,
-    User,
-    Settings,
-    Building2,
-} from "lucide-react";
+import { navItems } from "@/data/globals";
 export default function Authenticated({ user, header, children }) {
     const [showingNavigationDropdown, setShowingNavigationDropdown] =
         useState(false);
-
     return (
         /*
             <div className='flex  flex-row-reverse md:flex-col'>
@@ -138,7 +128,7 @@ export default function Authenticated({ user, header, children }) {
             <div
                 className={
                     (showingNavigationDropdown ? "hidden" : "block") +
-                    " lg:block w-[370px]  dark:bg-gray-800"
+                    " lg:block w-[370px]  dark:bg-gray-800 sm:hidden"
                 }
             >
                 <div className="flex flex-col">
@@ -150,69 +140,17 @@ export default function Authenticated({ user, header, children }) {
                         </div>
                     </div>
                     <ul className="flex flex-col space-y-3 mt-[110px]">
-                        <NavLink
-                            href={route("dashboard")}
-                            active={route().current("")}
-                        >
-                            <span className="flex flex-row items-center gap-2">
-                                <CircleGauge />
-                                Dashboard
-                            </span>
-                        </NavLink>
-                        <NavLink
-                            href={route("dashboard")}
-                            active={route().current("users")}
-                        >
-                            <span className="flex flex-row items-center gap-2">
-                                <FilePen />
-                                Travel Request
-                            </span>
-                        </NavLink>
-                        <NavLink
-                            href={route("dashboard")}
-                            active={route().current("travel-order")}
-                        >
-                            <span className="flex flex-row items-center gap-2">
-                                <Compass />
-                                Travel Order
-                            </span>
-                        </NavLink>
-                        <NavLink
-                            href={route("dashboard")}
-                            active={route().current("employees")}
-                        >
-                            <span className="flex flex-row items-center gap-2">
-                                <Users />
-                                Employees
-                            </span>
-                        </NavLink>
-                        <NavLink
-                            href={route("dashboard")}
-                            active={route().current("divisions")}
-                        >
-                            <span className="flex flex-row items-center gap-2">
-                                <Building2 />
-                                Divisions
-                            </span>
-                        </NavLink>
-                        <NavLink
-                            href={route("dashboard")}
-                            active={route().current("users")}
-                        >
-                            <span className="flex flex-row items-center gap-2">
-                                <User />
-                                Users
-                            </span>
-                        </NavLink>
-                        <NavLink
-                            href={route("dashboard")}
-                            active={route().current("users")}
-                        >
-                            <span className="flex flex-row items-center gap-2">
-                                <Settings />
-                                Settings
-                            </span>
-                        </NavLink>
+                        {navItems.map((item, key) => (
+                            <NavLink
+                                key={key}
+                                className="gap-2"
+                                href={item.link}
+                                active={route().current(item.link)}
+                            >
+                                {<item.icon />}
+                                {item.name}
+                            </NavLink>
+                        ))}
                     </ul>
                 </div>
             </div>
@@ -224,7 +162,7 @@ export default function Authenticated({ user, header, children }) {
                                 (previousState) => !previousState
                             )
                         }
-                        className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 dark:text-gray-500 hover:text-gray-500 dark:hover:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-900 focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-900 focus:text-gray-500 dark:focus:text-gray-400 transition duration-150 ease-in-out"
+                        className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 dark:text-gray-800 hover:text-gray-800 dark:hover:text-gray-800 hover:bg-gray-100  focus:outline-none focus:bg-gray-100  focus:text-gray-800 dark:focus:text-gray-800 transition duration-150 ease-in-out"
                     >
                         <svg
                             className="h-6 w-6"
@@ -256,13 +194,54 @@ export default function Authenticated({ user, header, children }) {
                             />
                         </svg>
                     </button>
-                    <div>{user.name}</div>
+                    <div>
+                        <div className=" sm:items-center sm:ms-6">
+                            <div className="ms-3 relative">
+                                <Dropdown>
+                                    <Dropdown.Trigger>
+                                        <span className="inline-flex rounded-md">
+                                            <button
+                                                type="button"
+                                                className="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150"
+                                            >
+                                                {user.name}
+
+                                                <svg
+                                                    className="ms-2 -me-0.5 h-4 w-4"
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                    viewBox="0 0 20 20"
+                                                    fill="currentColor"
+                                                >
+                                                    <path
+                                                        fillRule="evenodd"
+                                                        d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                                        clipRule="evenodd"
+                                                    />
+                                                </svg>
+                                            </button>
+                                        </span>
+                                    </Dropdown.Trigger>
+
+                                    <Dropdown.Content>
+                                        <Dropdown.Link
+                                            href={route("profile.edit")}
+                                        >
+                                            Profile
+                                        </Dropdown.Link>
+                                        <Dropdown.Link
+                                            href={route("logout")}
+                                            method="post"
+                                            as="button"
+                                        >
+                                            Log Out
+                                        </Dropdown.Link>
+                                    </Dropdown.Content>
+                                </Dropdown>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div>dfgdk</div>
-                <main className="bg-gray-200">
-                    {children}
-                    <p>children</p>
-                </main>
+                <main className="bg-gray-200">{children}</main>
             </div>
         </div>
     );
