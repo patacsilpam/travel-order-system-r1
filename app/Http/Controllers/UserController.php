380bf0411eab9ee42535a905cf468617aaa3cc01
patacsilpam <pamelampatacsil@gmail.com>
2024-07-24 16:01:57 +0800

    UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Redirect;
+use Inertia\Inertia;
+use Inertia\Response;
 class UserController extends Controller
 {
     //
