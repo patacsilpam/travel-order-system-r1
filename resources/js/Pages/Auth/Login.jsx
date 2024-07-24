475bea91016c60d3cc28b8df664c565fa4c004d4
patacsilpam <pamelampatacsil@gmail.com>
2024-07-24 16:02:55 +0800

    login

--- resources/js/Pages/Auth/Login.jsx
+++ resources/js/Pages/Auth/Login.jsx
@@ -1,46 +1,56 @@
-import { useEffect } from 'react';
-import Checkbox from '@/Components/Checkbox';
-import GuestLayout from '@/Layouts/GuestLayout';
-import InputError from '@/Components/InputError';
-import InputLabel from '@/Components/InputLabel';
-import PrimaryButton from '@/Components/PrimaryButton';
-import TextInput from '@/Components/TextInput';
-import { Head, Link, useForm } from '@inertiajs/react';
+import { useEffect } from "react";
+import Checkbox from "@/Components/Checkbox";
+import GuestLayout from "@/Layouts/GuestLayout";
+import InputError from "@/Components/InputError";
+import InputLabel from "@/Components/InputLabel";
+import PrimaryButton from "@/Components/PrimaryButton";
+import TextInput from "@/Components/TextInput";
+import { Head, Link, useForm } from "@inertiajs/react";
 
 export default function Login({ status, canResetPassword }) {
     const { data, setData, post, processing, errors, reset } = useForm({
-        email: '',
-        password: '',
+        email: "",
+        password: "",
         remember: false,
     });
 
     useEffect(() => {
         return () => {
-            reset('password');
+            reset("password");
         };
     }, []);
 
     const submit = (e) => {
         e.preventDefault();
 
-        post(route('login'));
+        post(route("login"));
     };
 
     return (
         <GuestLayout>
             <Head title="Log in" />
 
-            {status && <div className="mb-4 font-medium text-sm text-green-600">{status}</div>}
-            <div className='flex flex-row p-2 bg-[#2B2D37] text-white gap-2'>
+            {status && (
+                <div className="mb-4 font-medium text-sm text-green-600">
+                    {status}
+                </div>
+            )}
+            <div className="flex flex-row p-2 bg-[#2B2D37] text-white gap-2">
                 <div>
-                    <img src="/images/logo.png" alt='NCIP Logo' className='w-[50px]'/>
+                    <img
+                        src="/images/logo.png"
+                        alt="NCIP Logo"
+                        className="w-[50px]"
+                    />
                 </div>
                 <div>
-                    <small className='text-[#E7E7E7]'>National Commission on Indigenous Peoples Region I</small>
+                    <small className="text-[#E7E7E7]">
+                        National Commission on Indigenous Peoples Region I
+                    </small>
                     <h1>EMPLOYEE TRAVEL ORDER SYSTEM</h1>
                 </div>
             </div>
-            <form onSubmit={submit} className='p-4'>
+            <form onSubmit={submit} className="p-4">
                 <div>
                     <InputLabel htmlFor="email" value="Email" />
                     <TextInput
@@ -52,7 +62,7 @@ export default function Login({ status, canResetPassword }) {
                         autoComplete="username"
                         isFocused={true}
                         placeholder="Enter email address"
-                        onChange={(e) => setData('email', e.target.value)}
+                        onChange={(e) => setData("email", e.target.value)}
                     />
 
                     <InputError message={errors.email} className="mt-2" />
@@ -69,7 +79,7 @@ export default function Login({ status, canResetPassword }) {
                         className="mt-1 block w-full "
                         autoComplete="current-password"
                         placeholder="Enter password"
-                        onChange={(e) => setData('password', e.target.value)}
+                        onChange={(e) => setData("password", e.target.value)}
                     />
 
                     <InputError message={errors.password} className="mt-2" />
@@ -80,9 +90,13 @@ export default function Login({ status, canResetPassword }) {
                         <Checkbox
                             name="remember"
                             checked={data.remember}
-                            onChange={(e) => setData('remember', e.target.checked)}
+                            onChange={(e) =>
+                                setData("remember", e.target.checked)
+                            }
                         />
-                        <span className="ms-2 text-sm text-gray-600 dark:text-gray-400">Remember me</span>
+                        <span className="ms-2 text-sm text-gray-600 dark:text-gray-400">
+                            Remember me
+                        </span>
                     </label>
                 </div>
 
