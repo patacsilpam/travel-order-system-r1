8857be8a450b112f6aacf1c1abca9c78f8f7fd95
patacsilpam <pamelampatacsil@gmail.com>
2024-07-27 00:10:28 +0800

    feat: title page

---
+++ resources/js/Pages/Settings/Show.jsx
@@ -0,0 +1,18 @@
+import { Head } from "@inertiajs/react";
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+const Show = ({ auth }) => {
+    return (
+        <div>
+            <AuthenticatedLayout
+                user={auth.user}
+                header={
+                    <h2 className="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight"></h2>
+                }
+            >
+                <Head title="Settings" />
+            </AuthenticatedLayout>
+        </div>
+    );
+};
+
+export default Show;
