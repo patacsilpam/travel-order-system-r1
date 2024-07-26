1e9952c1d1a624649279f3fb4894c05f39f31d47
patacsilpam <pamelampatacsil@gmail.com>
2024-07-27 00:10:59 +0800

    feat: title page

--- resources/js/Pages/User/Show.jsx
+++ resources/js/Pages/User/Show.jsx
@@ -1,8 +1,18 @@
-import React from "react";
-import PrimaryButton from "@/Components/PrimaryButton";
-import NavLink from "@/Components/NavLink";
-const Show = () => {
-    return <div>Users</div>;
+import { Head } from "@inertiajs/react";
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+const Show = ({ auth }) => {
+    return (
+        <div>
+            <AuthenticatedLayout
+                user={auth.user}
+                header={
+                    <h2 className="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight"></h2>
+                }
+            >
+                <Head title="User" />
+            </AuthenticatedLayout>
+        </div>
+    );
 };
 
 export default Show;
