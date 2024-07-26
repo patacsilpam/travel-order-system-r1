ff31ee738867ae2ff2c1648d9a881ce856cffcf7
patacsilpam <pamelampatacsil@gmail.com>
2024-07-27 00:10:17 +0800

    feat: title page

--- resources/js/Pages/Division/Show.jsx
+++ resources/js/Pages/Division/Show.jsx
@@ -1,7 +1,18 @@
-import React from "react";
-
-const Show = () => {
-    return <div>Division</div>;
+import { Head } from "@inertiajs/react";
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+const Show = ({ auth }) => {
+    return (
+        <div>
+            <AuthenticatedLayout
+                user={auth.user}
+                header={
+                    <h2 className="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight"></h2>
+                }
+            >
+                <Head title="Division" />
+            </AuthenticatedLayout>
+        </div>
+    );
 };
 
 export default Show;
