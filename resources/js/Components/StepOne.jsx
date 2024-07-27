ea6472c036d0145dd288d935ad9f346db3eb2203
patacsilpam <pamelampatacsil@gmail.com>
2024-07-27 23:34:00 +0800

    component: Step One

---
+++ resources/js/Components/StepOne.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import InputLabel from "./InputLabel";
+import TextInput from "./TextInput";
+
+const StepOne = ({ formData, setFormData }) => {
+    return (
+        <div>
+            <h2>Step 1</h2>
+            <TextInput
+                type="text"
+                placeholder="First Name"
+                className="container-fluid w-full"
+                value={formData.firstName}
+                onChange={(e) =>
+                    setFormData({ ...formData, firstName: e.target.value })
+                }
+            />
+        </div>
+    );
+};
+
+export default StepOne;
