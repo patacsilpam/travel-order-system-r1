c7c21c8422417d1f83f1c7ece97b8df1653d8e09
patacsilpam <pamelampatacsil@gmail.com>
2024-07-28 20:42:20 +0800

    component: authenticated layout

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -131,22 +131,23 @@ export default function Authenticated({ user, header, children }) {
         </div>
         */
 
-        <div className="flex flex-row justify-around h-screen bg-slate-100 ">
+        <div className="flex flex-row justify-around h-screen bg-slate-100">
             <div
                 className={
-                    (showingNavigationDropdown ? "hidden" : "block") +
-                    " md:block w-[370px]  dark:bg-gray-800 sm:hidden"
+                    (showingNavigationDropdown
+                        ? "flex absolute top-0 left-0 h-full z-50"
+                        : "hidden") + "   bg-gray-900 lg:flex"
                 }
             >
-                <div className="flex flex-col h-screen justify-around">
-                    <div className="flex flex-row items-center -mt-14 p-2 gap-2">
+                <div className="flex flex-col space-y-24">
+                    <div className="flex flex-row items-center p-2 gap-2 w-[300px]">
                         <img src="/images/logo.png" className="w-[60px]" />
                         <div className="text-white">
                             <small className="text-[#E7E7E7]">NCIP R1</small>
                             <p>TRAVEL ORDER SYSTEM</p>
                         </div>
                     </div>
-                    <ul className="flex flex-col space-y-3">
+                    <ul className="flex flex-col space-y-3 ">
                         <NavLink
                             href={route("dashboard")}
                             active={route().current("dashboard")}
@@ -187,20 +188,19 @@ export default function Authenticated({ user, header, children }) {
                             <User />
                             Users
                         </NavLink>
+                        <NavLink
+                            href={route("settings.show")}
+                            active={route().current("settings.show")}
+                            className="gap-2"
+                        >
+                            <Settings />
+                            Settings
+                        </NavLink>
                     </ul>
-
-                    <NavLink
-                        href={route("settings.show")}
-                        active={route().current("settings.show")}
-                        className="gap-2"
-                    >
-                        <Settings />
-                        Settings
-                    </NavLink>
                 </div>
             </div>
             <div className="w-screen">
-                <div className="flex flex-row items-center justify-between md:justify-end p-5 border border-b-gray-300 shadow-sm">
+                <div className="flex flex-row items-center justify-between lg:justify-end p-5 border border-b-gray-300 shadow-sm">
                     <button
                         onClick={() =>
                             setShowingNavigationDropdown(
@@ -242,51 +242,98 @@ export default function Authenticated({ user, header, children }) {
                     <div>
                         <div className="sm:items-center sm:ms-6">
                             <div className="ms-3 relative">
-                                <Dropdown>
-                                    <Dropdown.Trigger>
-                                        <span className="inline-flex rounded-md">
-                                            <button
-                                                type="button"
-                                                className="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150"
-                                            >
-                                                {user.name}
-
-                                                <svg
-                                                    className="ms-2 -me-0.5 h-4 w-4"
-                                                    xmlns="http://www.w3.org/2000/svg"
-                                                    viewBox="0 0 20 20"
-                                                    fill="currentColor"
+                                {showingNavigationDropdown ? (
+                                    <button
+                                        onClick={() =>
+                                            setShowingNavigationDropdown(
+                                                (previousState) =>
+                                                    !previousState
+                                            )
+                                        }
+                                        className="lg:hidden md:flex inline-flex items-center justify-center p-2 rounded-md text-gray-400 dark:text-gray-800 hover:text-gray-800 dark:hover:text-gray-800 hover:bg-gray-100  focus:outline-none focus:bg-gray-100  focus:text-gray-800 dark:focus:text-gray-800 transition duration-150 ease-in-out"
+                                    >
+                                        <svg
+                                            className="h-6 w-6"
+                                            stroke="currentColor"
+                                            fill="none"
+                                            viewBox="0 0 24 24"
+                                        >
+                                            <path
+                                                className={
+                                                    !showingNavigationDropdown
+                                                        ? "inline-flex"
+                                                        : "hidden"
+                                                }
+                                                strokeLinecap="round"
+                                                strokeLinejoin="round"
+                                                strokeWidth="2"
+                                                d="M4 6h16M4 12h16M4 18h16"
+                                            />
+                                            <path
+                                                className={
+                                                    showingNavigationDropdown
+                                                        ? "inline-flex"
+                                                        : "hidden"
+                                                }
+                                                strokeLinecap="round"
+                                                strokeLinejoin="round"
+                                                strokeWidth="2"
+                                                d="M6 18L18 6M6 6l12 12"
+                                            />
+                                        </svg>
+                                    </button>
+                                ) : (
+                                    <Dropdown>
+                                        <Dropdown.Trigger>
+                                            <span className="inline-flex rounded-md">
+                                                <button
+                                                    type="button"
+                                                    className="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150"
                                                 >
-                                                    <path
-                                                        fillRule="evenodd"
-                                                        d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                                                        clipRule="evenodd"
-                                                    />
-                                                </svg>
-                                            </button>
-                                        </span>
-                                    </Dropdown.Trigger>
+                                                    {user.name}
+                                                    <svg
+                                                        className="ms-2 -me-0.5 h-4 w-4"
+                                                        xmlns="http://www.w3.org/2000/svg"
+                                                        viewBox="0 0 20 20"
+                                                        fill="currentColor"
+                                                    >
+                                                        <path
+                                                            fillRule="evenodd"
+                                                            d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                                            clipRule="evenodd"
+                                                        />
+                                                    </svg>
+                                                </button>
+                                            </span>
+                                        </Dropdown.Trigger>
 
-                                    <Dropdown.Content>
-                                        <Dropdown.Link
-                                            href={route("profile.edit")}
-                                        >
-                                            Profile
-                                        </Dropdown.Link>
-                                        <Dropdown.Link
-                                            href={route("logout")}
-                                            method="post"
-                                            as="button"
-                                        >
-                                            Log Out
-                                        </Dropdown.Link>
-                                    </Dropdown.Content>
-                                </Dropdown>
+                                        <Dropdown.Content>
+                                            <Dropdown.Link
+                                                href={route("profile.edit")}
+                                            >
+                                                Profile
+                                            </Dropdown.Link>
+                                            <Dropdown.Link
+                                                href={route("logout")}
+                                                method="post"
+                                                as="button"
+                                            >
+                                                Log Out
+                                            </Dropdown.Link>
+                                        </Dropdown.Content>
+                                    </Dropdown>
+                                )}
                             </div>
                         </div>
                     </div>
                 </div>
-                <main>{children}</main>
+                <main
+                    className={
+                        showingNavigationDropdown ? "backdrop-opacity-70 " : ""
+                    }
+                >
+                    {children}
+                </main>
             </div>
         </div>
     );
