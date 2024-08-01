9cac67e0c0e3362eeecbac92483c417456eb3dd7
Pam Patacsil <74691394+patacsilpam@users.noreply.github.com>
2024-08-01 13:26:21 +0800

    Table

---
+++ resources/js/Components/Table.jsx
@@ -0,0 +1,35 @@
+import DataTable from "react-data-table-component";
+
+const columns = [
+    {
+        name: "Title",
+        selector: (row) => row.title,
+    },
+    {
+        name: "Year",
+        selector: (row) => row.year,
+    },
+];
+
+const data = [
+    {
+        id: 1,
+        title: "Beetlejuice",
+        year: "1988",
+    },
+    {
+        id: 2,
+        title: "Ghostbusters",
+        year: "1984",
+    },
+];
+
+const Table = ({ columns, data }) => {
+    return (
+        <div>
+            <DataTable columns={columns} data={data} selectableRows />
+        </div>
+    );
+};
+
+export default Table;
