28f947022b2fd8cde00fca09b852bb578aac2707
Pam Patacsil <74691394+patacsilpam@users.noreply.github.com>
2024-08-01 13:26:47 +0800

    feat: user page

--- resources/js/Pages/User/Show.jsx
+++ resources/js/Pages/User/Show.jsx
@@ -1,5 +1,29 @@
-import { Head } from "@inertiajs/react";
+import { Head, Link } from "@inertiajs/react";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+import DataTable from "react-data-table-component";
+const columns = [
+    {
+        name: "Title",
+        selector: (row) => row.title,
+    },
+    {
+        name: "Year",
+        selector: (row) => row.year,
+    },
+];
+
+const data = [
+    {
+        id: 1,
+        title: "Beetlejuice",
+        year: "1988",
+    },
+    {
+        id: 2,
+        title: "Ghostbusters",
+        year: "1984",
+    },
+];
 const Show = ({ auth }) => {
     return (
         <div>
@@ -10,6 +34,29 @@ const Show = ({ auth }) => {
                 }
             >
                 <Head title="User" />
+                <div className="m-5">
+                    <h1 className="text-4xl font-bold my-5">User</h1>
+                    <div className="flex justify-between flex-col lg:flex-row">
+                        <div>
+                            <span>Insert Filter Here</span>
+                        </div>
+                        <div>
+                            <Link
+                                href=""
+                                className="bg-blue-600 bg-opacity-90 hover:bg-blue-700 text-white p-2 rounded-md w-[200px] text-center"
+                            >
+                                <strong>Create User</strong>
+                            </Link>
+                        </div>
+                    </div>
+                    <div className="bg-slate-200">
+                        <DataTable
+                            columns={columns}
+                            data={data}
+                            selectableRows
+                        />
+                    </div>
+                </div>
             </AuthenticatedLayout>
         </div>
     );
