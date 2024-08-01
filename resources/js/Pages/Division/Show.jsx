7ca04dba0029d4d557d19e955fc6a926061e8951
Pam Patacsil <74691394+patacsilpam@users.noreply.github.com>
2024-08-01 16:14:59 +0800

    feat: show division page

--- resources/js/Pages/Division/Show.jsx
+++ resources/js/Pages/Division/Show.jsx
@@ -1,5 +1,42 @@
-import { Head } from "@inertiajs/react";
+import { Head, Link } from "@inertiajs/react";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+import DataTable from "react-data-table-component";
+import DataTableExtensions from "react-data-table-component-extensions";
+const columns = [
+    {
+        id: "Number",
+        selector: (row) => row.id,
+    },
+    {
+        name: "Title",
+        selector: (row) => row.title,
+    },
+    {
+        name: "Year",
+        selector: (row) => row.year,
+    },
+];
+
+const data = [
+    {
+        id: 1,
+        title: "Beetlejuice",
+        year: "1988",
+    },
+    {
+        id: 2,
+        title: "Ghostbusters",
+        year: "1984",
+    },
+];
+const customStyles = {
+    headCells: {
+        style: {
+            fontSize: "1rem",
+            fontWeigth: "900",
+        },
+    },
+};
 const Show = ({ auth }) => {
     return (
         <div>
@@ -9,8 +46,35 @@ const Show = ({ auth }) => {
                     <h2 className="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight"></h2>
                 }
             >
-                <Head title="Division" />
-                <main>asdfasdfasdjkhfj</main>
+                <Head title="User" />
+                <div className="m-5">
+                    <h1 className="text-4xl font-bold my-5">Offices</h1>
+                    <div className="flex justify-end">
+                        <Link
+                            href={route("user.create")}
+                            className="bg-blue-600 bg-opacity-90 hover:bg-blue-700 text-white p-2 rounded-md w-[100px] text-center relative"
+                        >
+                            <strong>Add Office</strong>
+                        </Link>
+                    </div>
+                    <div className="relative">
+                        <DataTableExtensions
+                            columns={columns}
+                            data={data}
+                            print={false}
+                            export={false}
+                        >
+                            <DataTable
+                                fixedHeader={true}
+                                pagination
+                                highlightOnHover
+                                customStyles={customStyles}
+                                responsive={true}
+                                className="pt-3"
+                            />
+                        </DataTableExtensions>
+                    </div>
+                </div>
             </AuthenticatedLayout>
         </div>
     );
