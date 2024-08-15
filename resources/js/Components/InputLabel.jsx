45e6620b9b588483adfc9250a601c64f83a6392a
Pam Patacsil <74691394+patacsilpam@users.noreply.github.com>
2024-08-15 16:58:53 +0800

    feat: components

--- resources/js/Components/InputLabel.jsx
+++ resources/js/Components/InputLabel.jsx
@@ -1,6 +1,17 @@
-export default function InputLabel({ value, className = '', children, ...props }) {
+export default function InputLabel({
+    value,
+    className = "",
+    children,
+    ...props
+}) {
     return (
-        <label {...props} className={`block font-medium text-sm text-gray-700 dark:text-gray-400 ` + className}>
+        <label
+            {...props}
+            className={
+                `block font-medium text-sm text-gray-700 dark:text-gray-600 ` +
+                className
+            }
+        >
             {value ? value : children}
         </label>
     );
--- resources/js/Components/TextInput.jsx
+++ resources/js/Components/TextInput.jsx
@@ -17,7 +17,7 @@ export default forwardRef(function TextInput(
             {...props}
             type={type}
             className={
-                "border-gray-300 dark:border-gray-700  dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm" +
+                "border-gray-300 dark:border-gray-700  dark:text-gray-700 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm" +
                 className
             }
             ref={input}
