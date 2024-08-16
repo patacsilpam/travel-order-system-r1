8053c6176908289af60a154fa440f8f15e4948c1
Pam Patacsil <74691394+patacsilpam@users.noreply.github.com>
2024-08-16 15:27:17 +0800

    feat: travel request

--- resources/js/Pages/TravelRequest/Show.jsx
+++ resources/js/Pages/TravelRequest/Show.jsx
@@ -1,6 +1,43 @@
-import { Head } from "@inertiajs/react";
+import { Head, Link } from "@inertiajs/react";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
-import WizardForm from "@/Components/WizardForm";
+import DataTable from "react-data-table-component";
+import DataTableExtensions from "react-data-table-component-extensions";
+import { CirclePlus } from "lucide-react";
+const columns = [
+    {
+        id: "Number",
+        selector: (row) => row.id,
+    },
+    {
+        name: "Title",
+        selector: (row) => row.title,
+    },
+    {
+        name: "Year",
+        selector: (row) => row.year,
+    },
+];
+
+const data = [
+    {
+        id: 1,
+        title: "Beetlejuice",
+        year: "1988",
+    },
+    {
+        id: 2,
+        title: "Ghostbusters",
+        year: "1984",
+    },
+];
+const customStyles = {
+    headCells: {
+        style: {
+            fontSize: "1rem",
+            fontWeigth: "900",
+        },
+    },
+};
 const Show = ({ auth }) => {
     return (
         <div className="relative">
@@ -11,8 +48,40 @@ const Show = ({ auth }) => {
                 }
             >
                 <Head title="Travel Request" />
-                <div className="bg-white m-10 shadow-sm relative">
+                {/*<div className="bg-white m-10 shadow-sm relative">
                     <WizardForm />
+                </div>*/}
+                <div className="m-5">
+                    <h1 className="text-4xl font-bold my-5">User</h1>
+                    <div className="flex  md:justify-end sm:justify-start mb-1">
+                        <div className="flex justify-center bg-blue-600 bg-opacity-90 hover:bg-blue-700 text-white p-2 rounded-md  text-center space-x-3">
+                            <div>
+                                <CirclePlus />
+                            </div>
+                            <div>
+                                <Link href={route("user.create")}>
+                                    <span>Add New User</span>
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="relative">
+                        <DataTableExtensions
+                            columns={columns}
+                            data={data}
+                            print={false}
+                            export={false}
+                        >
+                            <DataTable
+                                fixedHeader={true}
+                                pagination
+                                highlightOnHover
+                                customStyles={customStyles}
+                                responsive={true}
+                                className="pt-3"
+                            />
+                        </DataTableExtensions>
+                    </div>
                 </div>
             </AuthenticatedLayout>
         </div>
