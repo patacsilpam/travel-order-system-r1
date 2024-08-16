f7eba88d1d31d74a4e54dceb8b6057c6e836061b
Pam Patacsil <74691394+patacsilpam@users.noreply.github.com>
2024-08-16 15:26:56 +0800

    feat: GuestLayout page

--- resources/js/Layouts/GuestLayout.jsx
+++ resources/js/Layouts/GuestLayout.jsx
@@ -3,14 +3,14 @@ import { Link } from "@inertiajs/react";
 
 export default function Guest({ children }) {
     return (
-        <div className="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0  ">
+        <div className="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0  bg-neutral-200 p-2">
             <div>
                 <Link href="/">
                     <ApplicationLogo className="w-20 h-20 fill-current text-gray-500" />
                 </Link>
             </div>
 
-            <div className="w-full sm:max-w-md bg-white border border-[#D6D6D6] overflow-hidden sm:rounded-lg">
+            <div className="h-96 w-full sm:max-w-md bg-neutral-100 border border-[#D6D6D6] overflow-hidden rounded-sm   ">
                 {children}
             </div>
         </div>
