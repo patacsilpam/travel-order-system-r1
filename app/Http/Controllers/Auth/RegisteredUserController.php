44c02ad204246ae28d1b73a56054d46578b4449a
Pam Patacsil <74691394+patacsilpam@users.noreply.github.com>
2024-08-20 16:43:48 +0800

    feat:  added other input fields

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -31,13 +31,19 @@ class RegisteredUserController extends Controller
     public function store(Request $request): RedirectResponse
     {
         $request->validate([
-            'name' => 'required|string|max:255',
+            'firstName' => 'required|string|max:255',
+            'lastName' => 'required|string|max:255',
+            'position' => 'required|string|max:255',
+            'office' => 'required|string|max:255',
             'email' => 'required|string|lowercase|email|max:255|unique:'.User::class,
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
         $user = User::create([
-            'name' => $request->name,
+            'firstName' => $request->firstName,
+            'lastName' => $request->lastName,
+            'position' => $request->position,
+            'office' => $request->office,
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
