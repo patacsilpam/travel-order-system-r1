3beb5897eb45f76f2b87692d50db25758ddeebfa
Pam Patacsil <74691394+patacsilpam@users.noreply.github.com>
2024-08-20 16:44:16 +0800

    feat: layout

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -290,7 +290,7 @@ export default function Authenticated({ user, header, children }) {
                                                     type="button"
                                                     className="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150"
                                                 >
-                                                    {user.name}
+                                                    {`${user.firstName} ${user.lastName}`}
                                                     <svg
                                                         className="ms-2 -me-0.5 h-4 w-4"
                                                         xmlns="http://www.w3.org/2000/svg"
