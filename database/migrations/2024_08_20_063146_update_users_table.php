1beb89c7a496d81edb5be9267637a3b86f919ade
Pam Patacsil <74691394+patacsilpam@users.noreply.github.com>
2024-08-20 16:45:23 +0800

    migration table

---
+++ database/migrations/2024_08_20_063146_update_users_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+            $table->string('firstName')->after('name');
+            $table->string('lastName')->after('firstName');
+            $table->string('position')->after('lastName');
+            $table->string('office')->after('position');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+};
