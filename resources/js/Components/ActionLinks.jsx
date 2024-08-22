c67a4532b39916c0b457e925d253814ec390ceb3
Pam Patacsil <74691394+patacsilpam@users.noreply.github.com>
2024-08-22 15:17:55 +0800

    feat: action links

---
+++ resources/js/Components/ActionLinks.jsx
@@ -0,0 +1,31 @@
+// resources/js/Components/ActionLinks.jsx
+import React from "react";
+import { Link } from "@inertiajs/react";
+
+const ActionLinks = ({
+    row,
+    editUrl,
+    deleteUrl,
+    editLabel = "Edit",
+    deleteLabel = "Delete",
+}) => {
+    return (
+        <div className="flex lg:flex-row flex-col gap-3">
+            <Link
+                href={editUrl(row)}
+                className="bg-green-600 text-whi p-1 rounded-sm "
+            >
+                {editLabel}
+            </Link>
+
+            <Link
+                href={deleteUrl(row)}
+                className="bg-red-600 text-white p-1  rounded-sm "
+            >
+                {deleteLabel}
+            </Link>
+        </div>
+    );
+};
+
+export default ActionLinks;
