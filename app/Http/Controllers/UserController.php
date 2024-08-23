6f6d59fdd1c643084542f9d85e5a71d3f4e50afa
patacsilpam <pamelampatacsil@gmail.com>
2024-08-23 23:03:14 +0800

    controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,15 +1,11 @@
 <?php
 
 namespace App\Http\Controllers;
-
-use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Redirect;
 use Inertia\Inertia;
-use Inertia\Response;
 use Illuminate\Support\Facades\Hash;
 use App\Models\User;
+use Illuminate\Support\Str;
 class UserController extends Controller
 {
     //display data from db
@@ -29,15 +25,18 @@ class UserController extends Controller
         'lastName' => 'required|string|max:255',
         'position' => 'required|string|max:255',
         'office' => 'required|string|max:255',
+        'role' => 'required|string|max:255',
         'email' => 'required|string|email|max:255|unique:users,email',
         'password' => 'required|string|min:8',
        ]);
         // Create a new user record
      User::create([
-        'firstName' => $validateUser['firstName'],
-        'lastName' => $validateUser['lastName'],
+        'user_id' => Str::random(9),
+        'first_name' => $validateUser['firstName'],
+        'last_name' => $validateUser['lastName'],
         'position' => $validateUser['position'],
         'office' => $validateUser['office'],
+        'role' => $validateUser['role'],
         'email' => $validateUser['email'],
         'password' => Hash::make($validateUser['password']), // Hash the password before saving
     ]);
