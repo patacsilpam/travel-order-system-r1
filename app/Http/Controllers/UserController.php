b396f6a10822a4535ad5f3fc86f731960bac1ab5
patacsilpam <pamelampatacsil@gmail.com>
2024-08-25 23:55:21 +0800

    feat: controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,6 +6,7 @@ use Inertia\Inertia;
 use Illuminate\Support\Facades\Hash;
 use App\Models\User;
 use Illuminate\Support\Str;
+use Illuminate\Validation\Rules;
 class UserController extends Controller
 {
     //display data from db
@@ -31,7 +32,7 @@ class UserController extends Controller
        ]);
         // Create a new user record
      User::create([
-        'user_id' => Str::random(9),
+        'user_id' => Str::uuid()->toString(),
         'first_name' => $validateUser['firstName'],
         'last_name' => $validateUser['lastName'],
         'position' => $validateUser['position'],
@@ -49,21 +50,32 @@ class UserController extends Controller
     }
     public function edit(Request $request,$id){
         // Validate the incoming request data
-        $validated = $request->validate([
-            'firstName' => 'required|string|max:255',
-            'lastName' => 'required|string|max:255',
+        $request->validate([
+            'first_name' => 'required|string|max:255',
+            'last_name' => 'required|string|max:255',
             'position' => 'required|string|max:255',
             'office' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:users,email,' . $id,
-            'password' => 'required|string|min:8'
-            // Add other fields you want to update
+            'role' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users,email,' . $id, // Ignore unique check on the current user
+            'password' => ['nullable', 'confirmed', Rules\Password::defaults()],
         ]);
         // Find the user by ID
         $user = User::findOrFail($id);
-
-      
-        $user->update($validated);
-
+        // Updating the user
+        $user->update([
+            'first_name' => $request->first_name,
+            'last_name' => $request->last_name,
+            'position' => $request->position,
+            'office' => $request->office,
+            'role' => $request->role,
+            'email' => $request->email,
+        ]);
+        // If password is provided, hash and update it
+        if ($request->filled('password')) {
+            $user->update([
+                'password' => Hash::make($request->password),
+            ]);
+        }
         // Redirect back with a success message
         return redirect()->back()->with('success', 'User updated successfully.');
     }
