6d5dd59edb54e9f6b525141244568f10f9164e5f
Pam Patacsil <74691394+patacsilpam@users.noreply.github.com>
2024-08-28 15:34:41 +0800

    feat: select dropdown

---
+++ resources/js/Components/SelectDropdown.jsx
@@ -0,0 +1,42 @@
+import React, { forwardRef, useEffect, useRef } from "react";
+
+const SelectDropdown = forwardRef(function SelectDropdown(
+    {
+        options = [],
+        defaultValue = "Select a position",
+        className = "",
+        isFocused = false,
+        ...props
+    },
+    ref
+) {
+    const selectRef = ref ? ref : useRef();
+
+    useEffect(() => {
+        if (isFocused) {
+            selectRef.current.focus();
+        }
+    }, [isFocused]);
+
+    return (
+        <select
+            {...props}
+            className={
+                "border-gray-300 dark:border-gray-700 dark:text-gray-700 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm " +
+                className
+            }
+            ref={selectRef}
+        >
+            <option value="" disabled>
+                {defaultValue}
+            </option>
+            {options.map((option, index) => (
+                <option key={index} value={option.value}>
+                    {option.label}
+                </option>
+            ))}
+        </select>
+    );
+});
+
+export default SelectDropdown;
