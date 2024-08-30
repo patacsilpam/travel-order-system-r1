9b58cf1c6005601058b9bc654e21bf19433addc2
Pam Patacsil <74691394+patacsilpam@users.noreply.github.com>
2024-08-30 13:48:20 +0800

    feat: office controller

--- app/Http/Controllers/DivisionController.php
+++ app/Http/Controllers/DivisionController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Position;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
@@ -10,7 +11,11 @@ class DivisionController extends Controller
 {
     //
     public function show(){
-        $position = Position::all();
-        return Inertia::render('Division/Show',['position' => $position]);
+        $user = User::orderBy('first_name', 'asc')->get();
+        return Inertia::render('Division/Show',['users' => $user]);
+    }
+    public function create(Request $request){
+        $request->validate(['name' => 'required|string|max:255',
+        'office_head' => 'required|string|max:255']);
     }
 }
