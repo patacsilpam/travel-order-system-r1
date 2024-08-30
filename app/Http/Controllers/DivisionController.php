20fe9b6628e632e50d098b6d50506710ba30496a
Pam Patacsil <74691394+patacsilpam@users.noreply.github.com>
2024-08-30 15:26:12 +0800

    controller

--- app/Http/Controllers/DivisionController.php
+++ app/Http/Controllers/DivisionController.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Position;
+use App\Models\Office;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 use Inertia\Inertia;
 
 class DivisionController extends Controller
@@ -15,7 +16,18 @@ class DivisionController extends Controller
         return Inertia::render('Division/Show',['users' => $user]);
     }
     public function create(Request $request){
-        $request->validate(['name' => 'required|string|max:255',
-        'office_head' => 'required|string|max:255']);
+        $validateOffice = $request->validate([
+            'name' => 'required|string|max:255',
+            'office_head' => 'required|string|max:255'
+        ]);
+
+        //add new office
+        Office::create([ 
+            'office_id' => Str::uuid()->toString(),
+            'name' => $validateOffice['name'],
+            'office_head' => $validateOffice['office_head']
+        ]);
+        
+        return back()->with('message', 'Student added successfully');
     }
 }
