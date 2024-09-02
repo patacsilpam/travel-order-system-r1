9b16e22f460e43cb096354ea3508614c673a1b21
Pam Patacsil <74691394+patacsilpam@users.noreply.github.com>
2024-09-02 15:08:54 +0800

    feat: show page

--- resources/js/Pages/Division/Show.jsx
+++ resources/js/Pages/Division/Show.jsx
@@ -24,7 +24,7 @@ const columns = [
         name: "Action",
         selector: (row) => (
             <div className="flex lg:flex-row flex-col py-2 gap-5">
-                <EditOfficeForm />
+                <EditOfficeForm office={row} />
                 <DeleteDivisionForm />
             </div>
         ),
