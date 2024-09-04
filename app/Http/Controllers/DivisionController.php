bb5cbe2581f2eb6d1525810104e4ae974143af60
Pam Patacsil <74691394+patacsilpam@users.noreply.github.com>
2024-09-04 16:13:11 +0800

    controller

--- app/Http/Controllers/DivisionController.php
+++ app/Http/Controllers/DivisionController.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Office;
-use App\Models\User;
+use App\Models\Position;
 use Illuminate\Http\Request;
-use Illuminate\Support\Str;
 use Inertia\Inertia;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+use App\Models\Office;
+use Illuminate\Support\Str;
+use Illuminate\Validation\Rules;
 
 class DivisionController extends Controller
 {
@@ -30,8 +33,29 @@ class DivisionController extends Controller
         ]);
         return back()->with('message', 'Student added successfully');
     }
-    public function edit(){
-        $office = Office::orderBy('name', 'asc')->get();
-        return Inertia::render('Division/EditDivisionForm',['office' => $office]);
+
+    /*public function editForm($id) {
+        $office = Office::findOrFail($id);
+        $user = User::orderBy('first_name','asc')->get();
+        return Inertia::render('Division/EditDivisionForm', ['office' => $office, 'user' => $user]);
+    }*/
+    public function editForm($id){
+        $office = Office::findOrFail($id);
+        $user = User::orderBy('first_name', 'asc')->get();
+        return Inertia::render('Division/Edit',['divisions' => $office,'users' => $user]);
+    }
+
+    public function edit(Request $request, $id){
+        $request->validate(['name' => 'required|string|max:255']);
+        $request->validate(['office_head' => 'required|string|max:255']);
+        $office = Office::findOrFail($id);
+        $office->update(['name' => $request->name,'office_head' => $request->office_head]);
+        return back()->with('message', 'Edited successfully');
+    }
+
+    public function destroy($id){
+        $office = Office::findOrFail($id);
+        $office->delete();
+        return back()->with('message', ' Deleted successfully');
     }
 }
