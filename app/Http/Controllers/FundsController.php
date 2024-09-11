fe6a6a8feeb16e362b62a6a3180903d8b000352f
Pam Patacsil <74691394+patacsilpam@users.noreply.github.com>
2024-09-11 15:39:26 +0800

    feat: controller for funds

--- app/Http/Controllers/FundsController.php
+++ app/Http/Controllers/FundsController.php
@@ -14,54 +14,47 @@ class FundsController extends Controller
      */
     public function index()
     {
-        return Inertia::render("Funds/Show");
+        $funds = Funds::orderBy('funds','asc')->get();
+        return Inertia::render("Funds/Show", ['funds' => $funds]);
     }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function create(Request $request)
     {
         //
+        $validateFunds =  $request->validate(['funds' => 'required|string|max:255']);
+        Funds::create(['funds' => $validateFunds['funds']]);
+        return back()->with('message', 'Student added successfully');
+
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Funds $funds)
-    {
-        //
-    }
+    
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Funds $funds)
+    public function edit(Request $request, $id)
     {
         //
+        $request->validate(['name' => 'required|string|max:255']);
+        $funds = Funds::findOrFail($id);
+        $funds->update(['funds' => $request->name]);
+        return back()->with('message', ' Edited successfully');
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Funds $funds)
-    {
-        //
-    }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Funds $funds)
+    public function destroy($id)
     {
         //
+        $funds = Funds::findOrFail($id);
+        $funds->delete();
+        return back()->with('message', ' Edited successfully');
+
+        
+
     }
 }
