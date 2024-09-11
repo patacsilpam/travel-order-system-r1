c29ad613d57bbdc027b8edcf27127e82fbacd563
Pam Patacsil <74691394+patacsilpam@users.noreply.github.com>
2024-09-11 15:43:24 +0800

    feat: list page for funds

--- resources/js/Pages/Funds/Show.jsx
+++ resources/js/Pages/Funds/Show.jsx
@@ -1,6 +1,29 @@
-import { Head } from "@inertiajs/react";
+import { Head, Link } from "@inertiajs/react";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
-const Show = ({ auth }) => {
+import DataTable from "react-data-table-component";
+import DataTableExtensions from "react-data-table-component-extensions";
+import { CirclePlus } from "lucide-react";
+import { customStyles } from "@/data/globals";
+import AddSoureFundsForm from "./Create";
+import EditFundsForm from "./Edit";
+import DeleteFundsForm from "./Delete";
+
+const columns = [
+    {
+        name: "Source of Funds",
+        selector: (row) => row.funds,
+    },
+    {
+        name: "Action",
+        selector: (row) => (
+            <div className="flex lg:flex-row flex-col py-2 gap-5">
+                <EditFundsForm funds={row} />
+                <DeleteFundsForm funds={row} />
+            </div>
+        ),
+    },
+];
+const Show = ({ auth, funds }) => {
     return (
         <div>
             <AuthenticatedLayout
@@ -9,7 +32,37 @@ const Show = ({ auth }) => {
                     <h2 className="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight"></h2>
                 }
             >
-                <Head title="Funds" />
+                <Head title="Positions" />
+                <div className="m-5">
+                    <h1 className="text-4xl font-bold my-5">Source of Funds</h1>
+                    <div className="flex  md:justify-end sm:justify-start mb-1">
+                        <div className="flex justify-center bg-blue-600 bg-opacity-90 hover:bg-blue-700 text-white p-2 rounded-md  text-center space-x-3">
+                            <div>
+                                <CirclePlus />
+                            </div>
+                            <div>
+                                <AddSoureFundsForm />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="relative">
+                        <DataTableExtensions
+                            columns={columns}
+                            data={funds}
+                            print={false}
+                            export={false}
+                        >
+                            <DataTable
+                                fixedHeader={true}
+                                pagination
+                                highlightOnHover
+                                customStyles={customStyles}
+                                responsive={true}
+                                className="pt-3"
+                            />
+                        </DataTableExtensions>
+                    </div>
+                </div>
             </AuthenticatedLayout>
         </div>
     );
